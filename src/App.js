3faa9493ad5f82ca178f5abce7a896dc393b07a7
Paresh Salunke <ParryPC@Pareshs-MacBook-Pro.local>
2017-02-20 14:31:35 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+import './App.css';
+import Header from './Components/Header';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Portfolio from './Components/Portfolio';
+import Testimonials from './Components/Testimonials';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+
+
+class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			foo: 'bar',
+			resumeData: {}
+		};
+	}
+
+	getResumeData() {
+		$.ajax({
+			url: 'http://localhost:3000/resumeData.json',
+			dataType: 'json',
+			cache: false,
+			success: function (data) {
+				this.setState({resumeData:data});
+			}.bind(this),
+			error: function (xhr, status, err) {
+				console.log(err);
+				alert(err);
+			}
+		});
+	}
+
+	componentDidMount() {
+		this.getResumeData();
+	}
+
+	render() {
+
+		console.log(this.state.resumeData);
+
+		return (
+			<div className="App">
+				<Header data={this.state.resumeData.main}/>
+				<About data={this.state.resumeData.about} />
+				<Resume/>
+				<Portfolio/>
+				<Testimonials/>
+				<Contact/>
+				<Footer/>
+			</div>
+		);
+	}
+}
+
+export default App;
